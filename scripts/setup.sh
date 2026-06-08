b9732145960954dca4427887a1f4735a79785545
woliver99 <oliver@maplenetwork.ca>
2026-06-08 15:15:42 -0400

    Fix setup script to ensure directory creation

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,6 +7,4 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-mkdir -p /etc/nixos/
-cd /etc/nixos/
 nix-shell -p git --run "git init && git submodule add https://github.com/woliver99/nixos-system.git nixos-system && git submodule update --init --recursive"
