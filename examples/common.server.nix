e74d6ef1005fab3b0666d66f50ab7dee6e218653
woliver99 <oliver@maplenetwork.ca>
2026-06-08 16:42:01 -0400

    Remove bootloader preset import

--- examples/common.server.nix
+++ examples/common.server.nix
@@ -6,7 +6,6 @@
 
     # -- Presets --
     #./nixos-system/presets/hardware/proxmox.nix # Import your hardware preset here
-    ./nixos-system/presets/bootloader/grub-uefi.nix # Import your bootloader preset here
     ./nixos-system/presets/users/woliver99.nix # Import your user preset here
     ./nixos-system/presets/editor/nixvim/ssh.nix # Import your editor preset here (nano is installed by default)
     ./nixos-system/presets/apps/essentials.nix # Import your apps preset here (options: essentials, full, developer)
