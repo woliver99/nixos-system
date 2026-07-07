5f014444386826a0f0b53687c2b6a2b532f8f0b9
Oliver Wuthrich-Giroux <oliver@maplenetwork.ca>
2026-07-07 15:59:22 -0400

    Update boot example

--- examples/boot.uefi-grub.nix
+++ examples/boot.uefi-grub.nix
@@ -3,7 +3,7 @@
 {
   imports = [
     ./nixos-system/features/grub-firmware-entry.nix # Adds a "Reboot to UEFI" entry
-    ./nixos-system/features/grub-dualboot.nix # Adds other operating systems (like Windows) to grub
+    #./nixos-system/features/grub-dualboot.nix # Adds other operating systems (like Windows) to grub
   ];
 
   boot.loader = {
