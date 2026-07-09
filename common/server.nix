e5167753a1356d147e8de1e92e28fbc1d24dc4bb
Oliver Wuthrich-Giroux <oliver@maplenetwork.ca>
2026-07-08 21:07:03 -0400

    Fix common server config references

--- common/server.nix
+++ common/server.nix
@@ -6,15 +6,15 @@
     ./default.nix # Common config for every system
 
     # -- Modules --
-    ./nixos-system/modules/profiles/apps/essentials.nix # Select your apps preset here (options: essentials, full, developer)
-    ./nixos-system/modules/editor/nixvim # Neovim set up with a custom config\
-    ./nixos-system/modules/users/woliver99.nix # Import your user preset here
+    ../modules/profiles/apps/essentials.nix # Select your apps preset here (options: essentials, full, developer)
+    ../modules/editor/nixvim # Neovim set up with a custom config\
+    ../modules/users/woliver99.nix # Import your user preset here
 
     # Features
-    ./nixos-system/modules/features/firewall.nix # Enables the system firewall (might break some apps if not configured properly)
-    #./nixos-system/modules/features/auto-update.nix # System auto updates and waits for a reboot to apply changes
-    ./nixos-system/modules/features/ssh.nix # Installs ssh only accessible via public keys (disables password logins)
-    ./nixos-system/modules/features/podman.nix
+    ../modules/features/firewall.nix # Enables the system firewall (might break some apps if not configured properly)
+    #../modules/features/auto-update.nix # System auto updates and waits for a reboot to apply changes
+    ../modules/features/ssh.nix # Installs ssh only accessible via public keys (disables password logins)
+    ../modules/features/podman.nix
   ];
 
   # Prevent OOM
