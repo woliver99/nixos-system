f4e52694e5c6a6cc8a103fdff789107292012a18
woliver99 <oliver@maplenetwork.ca>
2026-07-08 20:09:48 -0400

    Add pull_keys.sh script for SSH key setup

---
+++ scripts/pull_keys.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Meant for when inside nixos minimal installation
+# Can be pasted in proxmox with: sleep 2 && echo "type curl -sL https://raw.githubusercontent.com/woliver99/nixos-system/refs/heads/master/scripts/pull_keys.sh -o /tmp/pull_keys.sh && sudo bash /tmp/pull_keys.sh && rm /tmp/pull_keys.sh" | dotool
+set -e
+
+if [ "$EUID" -ne 0 ]; then
+  echo "❌ Please run this script as root (sudo)"
+  exit 1
+fi
+
+mkdir -p /root/.ssh && chmod 700 /root/.ssh
+curl https://github.com/woliver99.keys >> /root/.ssh/authorized_keys
+chmod 600 /root/.ssh/authorized_keys
